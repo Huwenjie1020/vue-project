8f84b83c817081b61126b9f8af68f2de49c5d2c1
Huwenjie1020 <1396881234@qq.com>
2020-12-31 17:42:44 +0800

    feat: 编程大赛项目

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,113 +1,78 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
-      >.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-          target="_blank"
-          rel="noopener"
-          >babel</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router"
-          target="_blank"
-          rel="noopener"
-          >router</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-          target="_blank"
-          rel="noopener"
-          >eslint</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-unit-jest"
-          target="_blank"
-          rel="noopener"
-          >unit-jest</a
-        >
-      </li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-          >Forum</a
-        >
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-          >Community Chat</a
-        >
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
-          >Twitter</a
-        >
-      </li>
-      <li>
-        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
-      </li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li>
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
-          >vue-router</a
-        >
-      </li>
-      <li>
-        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          rel="noopener"
-          >vue-devtools</a
-        >
-      </li>
-      <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
-          >vue-loader</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-          rel="noopener"
-          >awesome-vue</a
-        >
-      </li>
-    </ul>
+    <h1>{{ boxValueArr }}</h1>
   </div>
 </template>
 
 <script>
 export default {
   name: "HelloWorld",
-  props: {
-    msg: String
-  }
+  data() {
+    return {
+      boxValue: 0,
+      box: {
+        index: 1,
+        timestamp: 0,
+        currentHash: '',
+        lastHash: '0',
+        value: 0,
+
+      },
+      hash: 0,
+      boxValueArr: []
+    }
+  },
+  mounted() {
+    this.getBoxValue()
+  },
+  methods: {
+    //取hash值算法
+    hashNode(val){
+      const crypto = require('crypto');
+     return new Promise(resolve =>
+      setTimeout(
+        () =>
+          resolve(
+            crypto
+              .createHash('sha256')
+              .update(val)
+              .digest('hex')
+          ), 0)
+     )
+    },
+    getBoxValue() {
+      const crypto = require('crypto');      
+      let reg = /^(0{5})/;
+      let lastHash = 0;
+      let currentHash = 0;
+      for(let index = 1; index <= 10; index++) {
+        let boxValue = 0; 
+        // let timestamp = new Date().getTime();
+        console.log('-----currentHash', currentHash)
+        while(!reg.test(currentHash)) {
+          console.log('---', reg.test(currentHash));
+          boxValue++;
+          this.box = {
+            index,
+            // timestamp,
+            lastHash,
+            value: boxValue
+          }
+          const boxString = Object.values(this.box).join('')
+          // await this.hashNode(boxString).then((res) => {
+          //   console.log('------hash', res)
+          
+            currentHash = crypto
+              .createHash('sha256')
+              .update(boxString)
+              .digest('hex');
+          // })
+        }
+        lastHash = currentHash;
+        this.boxValueArr.push(boxValue)
+      }
+    }
+  },
 };
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,10 +1,24 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-
-Vue.config.productionTip = false;
-
-new Vue({
-  router,
-  render: h => h(App)
-}).$mount("#app");
+const crypto = require('crypto');      
+let reg = /^(0{5})/;
+let lastHash = 0;
+for (let index = 1; index <= 10; index++) {
+  let currentHash = 0;
+  let boxValue = 0;
+  // let timestamp = new Date().getTime();
+  while (!reg.test(currentHash)) {
+    boxValue++;
+    this.box = {
+      index,
+      // timestamp,
+      lastHash,
+      value: boxValue
+    }
+    const boxString = Object.values(this.box).join('')
+    currentHash = crypto
+      .createHash('sha256')
+      .update(boxString)
+      .digest('hex');
+  }
+  lastHash = currentHash;
+  console.log('-----神秘数字', boxValue)
+}
